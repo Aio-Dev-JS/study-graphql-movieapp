a0ed8b379ffa9bf034b6c2eff257691478449f82
Aio-Dev-JS <AioDev.JS@gmail.com>
2020-09-12 00:52:57 +0900

    lecture 1.1

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,26 @@
 import React from "react";
-export default () => <h1>Home</h1>;
+import { gql } from "apollo-boost"
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOIVES = gql`
+{
+    movies {
+        id
+        medium_cover_image
+    }
+}
+`;
+// 쿼리 작성
+
+
+export default () => {
+    console.log(useQuery(GET_MOIVES))
+    const { loading, error, data } = useQuery(GET_MOIVES);
+    // Hooks사용
+    if (loading) {
+        return "loading"
+    }
+    if (data && data.movies) {
+        return data.movies.map(m => <h1>{m.id}</h1>)
+    }
+};
